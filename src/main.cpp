a7ceca422736cd22a6a1a0481a920fcb85b679bc
piyush0001 <piyushsengar881@gmail.com>
2026-02-19 14:04:51 +0530

    Initialize Serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,20 @@
  * and detects obstacle presence.
  */
 
-// TODO 1:
-// Define IR sensor digital pin (Use pin 2)
+ // TODO 1:
+ // Define IR sensor digital pin (Use pin 2)
 const int IR_PIN = 2;
 
-// TODO 2:
-// Create variable to store sensor state
+
+ // TODO 2:
+ // Create variable to store sensor state
 int irState = 0;
 
 void setup() {
 
     // TODO 3:
     // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
     // TODO 4:
     // Configure IR pin as INPUT
